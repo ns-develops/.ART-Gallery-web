831aad1256487cf476223266b819567c8b415606
”ns-develops” <test.tt9061030@gmail.com>
2025-05-14 11:28:37 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ function App() {
   return (
     <Router basename="/.ART-Gallery-web">
       <div className="App">
+       
         <div className="corner-nav">
           <div className="logo-left">Favorable.</div>
+
+       
+          <div className="middle-nav">
+            <Link to="/" className="nav-link">Typewriter</Link>
+            <Link to="/app" className="nav-link">American</Link>
+            <Link to="/ideas" className="nav-link">Coffee</Link>
+          </div>
+
           <Link to="/contact" className="contact-link">Contact</Link>
         </div>
+
 
         <Routes>
           <Route path="/" element={<Home />} />
